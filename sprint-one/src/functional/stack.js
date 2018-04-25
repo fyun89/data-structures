d407d5e5c85e4ec23a8f48d22e2cd673255e51e4
Poisk <vpisotski@gmail.com>
2018-04-25 11:58:58 -0700

    up to stack-specific behavior +1

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,24 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
   someInstance.push = function(value) {
+    size++
+    storage.size = value;
   };
 
   someInstance.pop = function() {
+    size--
+    var result = storage.size;
+    delete storage.size;
+    return result
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
