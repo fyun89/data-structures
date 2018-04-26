d1db4d2c0a5de46c71cf79cd3f4eaaa692f9d925
Poisk <vpisotski@gmail.com>
2018-04-25 20:17:03 -0700

    started The functional-shared pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
+  var howOld = 0;
 
   someInstance.enqueue = function(value) {
+    storage[howOld] = value;
+    size++;
+    howOld++;
   };
 
   someInstance.dequeue = function() {
+    size--;
+
+    var firstItem = storage[Object.keys(storage)[0]];
+    delete storage[Object.keys(storage)[0]];
+    return firstItem;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
