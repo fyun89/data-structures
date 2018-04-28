713070ab66a77182b349d5f5be515ca025776ecb
fyun89 <fyun89@gmail.com>
2018-04-28 15:51:54 -0700

    Completed Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,49 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //debugger;
+  var n = new Tree(value);
+
+  // if (this.children.length > maxLength) {
+  //   this.children.children.push(n);
+  // } //replace back
+  this.children.push(n);
 };
 
 treeMethods.contains = function(target) {
-};
+  var trigger = false;
+  // while (trigger === false){
+  //   if (finder.contains(target)){
+  //     trigger = true
+  //   }else{
+  //     //some how add .children to the end of finder
+  //     finder = this.children.children
+  //   }
+  // }
 
+
+  _.each(this.children, function(el) {
+    if (el.value === target) {
+      trigger = true;
+    }else{
+      _.each(el.children, function(elem) {
+        if(elem.value === target){
+          trigger = true
+        }
+      });
+    }
+  });
+  return trigger;
+};
 
 
 /*
