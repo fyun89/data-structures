7d19034ce1a6db4c95c2e18eccd2c8be2fae2571
fyun89 <fyun89@gmail.com>
2018-04-28 21:43:39 -0700

    Update: Graph in progress - small updates

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -26,21 +26,39 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  var toSlice;
-  _.each(this.vertices, function(elem, i) {
-    if (_.indexOf(elem, node.value) !== -1 ) {
-      toSlice = i;
-    }
+  var targetIndex = null;
+  this.vertices.find(function(elem, i) {
+    console.log('target index ' + i);
+    targetIndex = i;
   });
-  var formal = this.vertices.slice(0, toSlice);
-  var latter = this.vertices.slice(toSlice + 1);
-  var combined = formal.concat(latter);
-  this.vertices = combined;
+  console.log('before delete ' + this.vertices);
+  this.vertices.splice(targetIndex, 1);
+  console.log('after delete ' + this.vertices);
+// debugger;
+//   var toSlice;
+//   var validation = false
+//   _.each(this.vertices, function(elem, i) {
+//     if (_.indexOf(elem, node) !== -1 ) {
+//       toSlice = i;
+//       validation = true
+//     }
+//   });
+//   var final;
+//   if (toSlice > 0){
+//     var formal = this.vertices.slice(0, toSlice);
+//     var latter = this.vertices.slice(toSlice + 1);
+//     var combined = formal.concat(latter);
+//   }else{
+    
+//   }
   
+//   this.vertices = combined;
+//   return validation
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  
 };
 
 // Connects two nodes in a graph by adding an edge between them.
