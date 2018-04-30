2c20db8c4e90d96acefe24a7835f2a573a6921d5
fyun89 <fyun89@gmail.com>
2018-04-29 17:27:18 -0700

    Completed Graph Data Structure

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,12 @@
 // Instantiate a new graph
 var Graph = function() {
   this.vertices = [];
-  //this.edges = [];
-  
-  
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   this.vertices.push([node]);
-  //this.edges.push([this.vertices.length-1, null]); //if hasEdge, chg null to that other end of edge?
+  console.log('addNode '+ node + ': ' + this.vertices)
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -27,93 +24,69 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   var targetExists = false;
-  //var nodeIndex = 
   for (var i = 0; i < this.vertices.length; i++) {
     if (this.vertices[i][0] === node) {
       this.vertices.splice(i, 1);
-      //targetExists = true
     }
-
-  }
-  for (var j = 0; j < this.vertices.length; j++){
-    for (var k = 1; k < this.vertices.length; k++) {
-      if (this.vertices[j][k] === node){
+  };
+  for (var j = 0; j < this.vertices.length; j++) {
+    for (var k = 0; k < this.vertices[j].length; k++) {
+      if (this.vertices[j][k] === node) {
         this.vertices[j].splice(k, 1);
       }
     }
-  }
-
-  // if (targetExists === true){
-    // for (var j = 0; j < this.vertices.length; j++){
-    //   if (this.vertices[j].includes(node)) {
-    //     for (var k = 0; k < this.vertices.length; k++) {
-    //       if (this.vertices[j][k] === node){
-    //         this.vertices[j].splice(k, 1)
-    //       }
-    //     }
-    //   }
-    // }
-  // }
-  //console.log('before delete ' + this.vertices);
-  //this.vertices.splice(targetIndex, 1);
-  //console.log('after delete ' + this.vertices);
-// debugger;
-//   var toSlice;
-//   var validation = false
-//   _.each(this.vertices, function(elem, i) {
-//     if (_.indexOf(elem, node) !== -1 ) {
-//       toSlice = i;
-//       validation = true
-//     }
-//   });
-//   var final;
-//   if (toSlice > 0){
-//     var formal = this.vertices.slice(0, toSlice);
-//     var latter = this.vertices.slice(toSlice + 1);
-//     var combined = formal.concat(latter);
-//   }else{
-    
-//   }
-  
-//   this.vertices = combined;
-//   return validation
+  };
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var finalCheck = false;
+  //var finalCheck = false;
   for (var i = 0; i < this.vertices.length; i++) {
-    var check = false;
+    var checkToNode = false;
+    var checkFromNode = false;
     if (this.vertices[i][0] === fromNode && this.vertices[i].includes(toNode)) {
-      check = true;
+      checkToNode = true;
     }
-    if (this.vertices[i][0] === toNode && this.vertices[i].includes(fromNode)) {
-      check = true;
+    // if (this.vertices[i][0] === toNode && this.vertices[i].includes(fromNode)) {
+    //   checkToNode = true;
+    // }
+    if (checkToNode === true /*&& checkFromNode === true*/) {
+      return true;
     }
-    finalCheck = check;
   }
-  return finalCheck;
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   for (var i = 0; i < this.vertices.length; i++) {
-    if (this.vertices[i][0] === fromNode && this.vertices[i].includes(toNode) === false) {
-      this.vertices[i].push(toNode)
+    if (this.vertices[i][0] === fromNode /*&& this.vertices[i].includes(toNode) === false*/) {
+      this.vertices[i].push(toNode);
+      console.log(this.vertices[i])
     }
-    if (this.vertices[i][0] === toNode && this.vertices[i].includes(fromNode) === false) {
-      this.vertices[i].push(fromNode)
+    if (this.vertices[i][0] === toNode && fromNode !== toNode /*this.vertices[i].includes(fromNode) === false*/) {
+      this.vertices[i].push(fromNode);
     }
   }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var edgeToDelete;
   for (var i = 0; i < this.vertices.length; i++) {
     if (this.vertices[i][0] === fromNode) {
       for (var j = 1; j < this.vertices[i].length; j++) {
-        if (this.vertices[i][j] === toNode)
-        this.vertices[i].splice(j, 1)
+        if (this.vertices[i][j] === toNode) {
+          edgeToDelete = this.vertices[i][0]
+          this.vertices[i].splice(j, 1);
+        }
+      }
+    }
+  }
+  for (var k = 0; k < this.vertices.length; k++){
+    for (var l = 1; l < this.vertices[k].length; l++){
+      if (this.vertices[k][l] === edgeToDelete){
+        this.vertices[k].splice(l, 1);
       }
     }
   }
@@ -121,6 +94,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  var arrayCopy = this.vertices.slice();
+  for (var i = 0; i < arrayCopy.length; i++){
+    cb.call(this, arrayCopy[i][0]);
+  }
+  //console.log('after: ' + this)
 };
 
 //var makeGraph = new Graph ()
